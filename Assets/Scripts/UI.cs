b07d50f21ca441444c9fee1145966fd7e8223827
8bitbadger <littlebear101@gmail.com>
2019-08-14 22:36:21 +0000

    Update

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -2,22 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class UI : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        GameObject.Destroy(gameObject);
+        
     }
 }
